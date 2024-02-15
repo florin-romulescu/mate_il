79b04c44df20a87b9fb6e33d0cc79b6701a42580
florinrm <romulescu.florin.dev@gmail.com>
2024-02-15 12:09:13 +0200

    Added admin site

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Post, Tag, Attachment
+
+class AdminSite(admin.AdminSite):
+    site_title = "MATE IL"
+
+admin_site = AdminSite(name="mate_il")
+
+class PostAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("Post Information", {"fields" : ["title", "author", "pub_date"]}),
+        ("Tags", {"fields" : ["tags"], "classes" : ["collapse"]}),
+        ("Attachments", {"fields" : ["attachments"], "classes" : ["collapse"]})
+    ]
 
 # Register your models here.
+admin_site.register(Post, PostAdmin)
+admin_site.register(Tag)
+admin_site.register(Attachment)
