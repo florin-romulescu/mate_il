fd4dd0a2747879f21ffbe7a4d9f4eb5124f9cbfc
florinrm <romulescu.florin.dev@gmail.com>
2024-02-15 12:24:12 +0200

    Redirect to blog application
    When accessing the index path it will redirect to the index page of the blog application.

--- mate_il/urls.py
+++ mate_il/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from blog.admin import admin_site
+from . import views
 
 urlpatterns = [
+    path("", views.index, name="index"),
     path("admin/", admin_site.urls),
-    path("blog/", include("blog.urls")),
+    path("blog/", include(("blog.urls", "blog"), namespace="blog")),
 ]
