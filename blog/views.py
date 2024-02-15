39b0c5794401327598ef7926dc76a56b02fa5862
florinrm <romulescu.florin.dev@gmail.com>
2024-02-15 12:10:56 +0200

    Added placeholders views

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+class IndexPage:
+    @staticmethod
+    def as_view(request):
+        return HttpResponse("<h1>Test</h1>")
+    
+class WorksheetPage:
+    @staticmethod
+    def as_view(request):
+        return HttpResponse("<h1>Worksheet</h1>")
+    
+class ExercisesPage:
+    @staticmethod
+    def as_view(request):
+        return HttpResponse("<h1>Exercises</h1>")
+    
+class BlogPage:
+    @staticmethod
+    def as_view(request):
+        return HttpResponse("<h1>Blog</h1>")
