013467b281a358ed7816097d1d2021ac4fd04106
Florin Romulescu <romulescu.florin.dev@gmail.com>
2024-05-31 11:09:14 +0300

    Allowing HOSTS for production

--- mate_il/settings.py
+++ mate_il/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-tt(y_spj)nl*(a+see6(-yaerh(@l!bppvpz$o4umt!vpv#x*f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
